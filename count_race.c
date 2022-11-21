338a96ce5b82b8b56b705ba6ca11d4f102738c86
ShadyHBedda <shadyhazem@gmail.com>
2022-11-21 03:32:30 +0200

    Working POSIX implementation of count_race.c

--- count_race.c
+++ count_race.c
@@ -1,33 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <omp.h>
 #include <pthread.h>
 
-int race_count(int *arrayInput, int arrayLength, int numberOfThreads) {
-    int T[numberOfThreads];
-    for (int i = 0; i < numberOfThreads; i++) {
-        T[i] = 0;
-    }
+int length = 100;
+int count;
+int count2;
+int *array;
+int numberOfThreads = 4;
+int increment = 0;
 
-    omp_set_num_threads(numberOfThreads);
-    int parallel_count = 0;
-    #pragma omp parallel
+int count1s()
+{
+    int i;
+    count = 0;
+    for (i = 0; i < length; i++)
     {
-        int i;
-        #pragma omp for
-        for (i=0; i<arrayLength; i++)
+        if (array[i] == 1)
         {
-            if (arrayInput[i] == 1)
-            {
-                parallel_count++;
-                T[omp_get_thread_num()]++;
-            }
+            count++;
         }
     }
+    return count;
+}
 
-    for (int i = 0; i < numberOfThreads; i++) {
-        printf("\nNumber of 1's found by Thread %d is %d", i, T[i]);
+void* count1s_parallel(void* arg) {
+    int index = *(int*)arg;
+    //printf("\nIndex: %d", index);
+    //printf("\nIncrement: %d", increment);
+    for (int i = index; i < index + increment; i++)
+    {
+        if (array[i] == 1)
+        {
+            count2++;
+            //printf("\nCount2: %d", count2);
+        }
     }
-    return parallel_count;
-};
+    return arg;
+}
+
+int main(void)
+{
+    srand(time(0));
+    int randArray[length];
+    for(int i=0;i<length;i++)
+        randArray[i]=rand()%6;   //Generate number between 0 and 5
+    array = &randArray;
+    /*
+    printf("\nElements of the array::");
+    for(int i=0;i<length;i++)
+    {
+        printf("\nElement number %d::%d",i+1,randArray[i]);
+    }
+    */
+    increment = length/numberOfThreads;
+    pthread_t th[numberOfThreads];
+    count = 0;
+    count2 = 0;
+    for (int i = 0; i < numberOfThreads; i++) {
+        int* a = malloc(sizeof(int));
+        *a = i * increment;
+        if (pthread_create(&th[i], NULL, &count1s_parallel, a) != 0) {
+            perror("Failed to create thread");
+        }
+    }
+    for (int i = 0; i < numberOfThreads; i++) {
+        int* r;
+        if (pthread_join(th[i], (void**) &r) != 0) {
+            perror("Failed to join thread");
+        }
+        free(r);
+    }
+    printf("\nNumber of 1's is %d", count1s());
+    printf("\nNumber of 1's counted by multithreading: %d", count2);
+    return 0;
+}
